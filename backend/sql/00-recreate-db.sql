ba5f2e55acb57e15b66f9fdd17756086a4d49741
duncan06 <tylerdownie0@gmail.com>
2023-07-29 15:54:50 -0700

    Adjust recreate file to not spit out extra warnings by using WITH (FORCE).

--- backend/sql/00-recreate-db.sql
+++ backend/sql/00-recreate-db.sql
@@ -1,6 +1,7 @@
 -- DEV ONLY - Comment out for keeping db between restart
-DROP DATABASE IF EXISTS app_db WITH (FORCE);
-DROP USER IF EXISTS app_user WITH (FORCE);
+select pg_terminate_backend(pid) from pg_stat_activity where usename = 'app_user';
+DROP DATABASE IF EXISTS app_db;
+DROP USER IF EXISTS app_user;
 
 -- DEV ONLY - for quick iteration
 CREATE USER app_user PASSWORD 'app_pwd_to_change';
